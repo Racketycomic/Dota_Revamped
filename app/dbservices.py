37345a40ab004e7296a664024648e27231cdb842
Vinay S <33209062+Racketycomic@users.noreply.github.com>
2021-06-12 09:29:04 +0530

    Update dbservices.py
    overhaul

--- app/dbservices.py
+++ app/dbservices.py
@@ -3,8 +3,9 @@ import pymongo.mongo_client
 import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-uri = "mongodb+srv://vinay_da:" + urllib.parse.quote("vinays@123")+"@dota2-agnuc.mongodb.net/test?retryWrites=true&w=majority"
+password=""
+username=""
+uri = "mongodb+srv://{username}:" + urllib.parse.quote("{password}")+"@dota2-agnuc.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(uri)
 player = client.dotaproj.player
 herodetails = client.dotaproj.herodetails
